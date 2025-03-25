c10d2a137fde51a22ccf8a9bd6dbd7e5c7e62a00
bisous <346045359@qq.com>
2025-03-26 00:56:09 +0800

    add mutex to set

--- utils/set.go
+++ utils/set.go
@@ -1,8 +1,11 @@
 package utils
 
+import "sync"
+
 // Set is a generic set, similar to the std::set in C++
 type Set[T comparable] struct {
-	data map[T]struct{}
+	data  map[T]struct{}
+	mutex sync.Mutex
 }
 
 // NewSet creates a new generic Set
@@ -14,26 +17,36 @@ func NewSet[T comparable]() *Set[T] {
 
 // Add adds an item to the set
 func (s *Set[T]) Add(item T) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	s.data[item] = struct{}{}
 }
 
 // Remove removes an item from the set
 func (s *Set[T]) Remove(item T) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	delete(s.data, item)
 }
 
 // Contains checks if an item exists in the set
 func (s *Set[T]) Contains(item T) bool {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	_, exists := s.data[item]
 	return exists
 }
 
 // Size returns the number of items in the set
 func (s *Set[T]) Size() int {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	return len(s.data)
 }
 
 func (s *Set[T]) GetItems() []T {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	keys := make([]T, 0, len(s.data))
 	for k := range s.data {
 		keys = append(keys, k)
@@ -43,15 +56,19 @@ func (s *Set[T]) GetItems() []T {
 
 func (s *Set[T]) Union(other *Set[T]) *Set[T] {
 	result := NewSet[T]()
-	for k := range s.data {
-		result.Add(k)
+
+	for _, item := range s.GetItems() {
+		result.Add(item)
 	}
-	for k := range other.data {
-		result.Add(k)
+	for _, item := range other.GetItems() {
+		result.Add(item)
 	}
+
 	return result
 }
 
 func (s *Set[T]) Clear() {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	s.data = make(map[T]struct{})
 }
